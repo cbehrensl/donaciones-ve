c25058dda23b540d8f9c0081c7bf5b81e73ddb4a
Omar Pava <omara.pava@farmatodo.com>
2026-06-26 18:12:11 -0500

    feat: ajuste cabeceras para iframe

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,27 @@
 import type { NextConfig } from "next";
 
+const ALLOWED_FRAME_ORIGINS = process.env.ALLOWED_FRAME_ORIGINS ?? "https://www.farmatodo.com.co https://www.farmatodo.com.ve";
+
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   compress: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `frame-ancestors ${ALLOWED_FRAME_ORIGINS}`,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
