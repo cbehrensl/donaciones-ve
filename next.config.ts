d9b406660dce7ed18c3c5dd3101ab6c40cdbd5e2
Omar Pava <omara.pava@farmatodo.com>
2026-06-30 11:28:38 -0500

    feat: se ajusta para acceder desde localhost via iframe

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const ALLOWED_FRAME_ORIGINS = process.env.ALLOWED_FRAME_ORIGINS ?? "https://www.farmatodo.com.co https://www.farmatodo.com.ve";
+const ALLOWED_FRAME_ORIGINS = process.env.ALLOWED_FRAME_ORIGINS ?? "https://www.farmatodo.com.co https://www.farmatodo.com.ve http://localhost:4200 http://localhost:3000";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
